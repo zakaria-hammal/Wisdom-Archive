d7ae10579adb0010d613f72e231015c1d40621a0
zakaria-hammal <hammalzakaria06@gmail.com>
2025-02-22 09:28:47 +0100

    Deployement

--- Back/quotes.sql
+++ Back/quotes.sql
@@ -8,8 +8,8 @@ CREATE TABLE "Quotes" (
 );
 
 
-INSERT INTO Quotes (Name, Country, QuoteBody) VALUES ("Confucius(Kong Qiu)", "China", "Our greatest glory is not in never falling, but in rising every time we fall");
-INSERT INTO Quotes (Name, Country, QuoteBody) VALUES ("Oscar Wilde", "Irland", "Be yourself; everyone else is already taken");
-INSERT INTO Quotes (Name, Country, QuoteBody) VALUES ("Mahatma Gandhi", "India", "Be the change that you wish to see in the world");
-INSERT INTO Quotes (Name, Country, QuoteBody) VALUES ("Marcus Aurelius", "Rome", "The soul becomes dyed with the color of its thoughts");
-INSERT INTO Quotes (Name, Country, QuoteBody) VALUES ("Dr. Seuss", "USA", "You know you're in love when you can't fall asleep because reality is finally better than your dreams");
+INSERT INTO Quotes (Name, Country, QuoteBody) VALUES ('Confucius(Kong Qiu)', 'China', 'Our greatest glory is not in never falling, but in rising every time we fall');
+INSERT INTO Quotes (Name, Country, QuoteBody) VALUES ('Oscar Wilde', 'Irland', 'Be yourself; everyone else is already taken');
+INSERT INTO Quotes (Name, Country, QuoteBody) VALUES ('Mahatma Gandhi', 'India', 'Be the change that you wish to see in the world');
+INSERT INTO Quotes (Name, Country, QuoteBody) VALUES ('Marcus Aurelius', 'Rome', 'The soul becomes dyed with the color of its thoughts');
+INSERT INTO Quotes (Name, Country, QuoteBody) VALUES ('Dr. Seuss', 'USA', 'You know you're in love when you can't fall asleep because reality is finally better than your dreams');
